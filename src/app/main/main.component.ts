cc4e76531a2c9b78ffe6d8384b0277ae46b3cd49
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-12-23 22:05:57 +0530

    finalized design main cmp and navbar sidebar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
